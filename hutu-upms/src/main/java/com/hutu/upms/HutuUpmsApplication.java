40a8072536ce19f667d2637e6626dbc21412d86c
hutu <806001926@qq.com>
2019-01-28 19:44:45 +0800

    页面样式优化

--- hutu-upms/src/main/java/com/hutu/upms/HutuUpmsApplication.java
+++ hutu-upms/src/main/java/com/hutu/upms/HutuUpmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 
 import static com.hutu.cache.constant.CacheConstant.CACHE_NAME;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableHutuCache
 public class HutuUpmsApplication {
