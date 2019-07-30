d744cda34235dd7e0e9565004b7b840a02db4b0e
minghauqiang <806001926@qq.com>
2019-07-30 13:55:06 +0800

    跨域设置优化

--- hutu-common/hutu-common-security/src/main/java/com/hutu/common/security/config/DefaultWebConfig.java
+++ hutu-common/hutu-common-security/src/main/java/com/hutu/common/security/config/DefaultWebConfig.java
@@ -15,6 +15,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -35,11 +38,25 @@ import java.util.List;
 public class DefaultWebConfig implements WebMvcConfigurer {
 
     /**
-     * 跨域配置
+     * 跨越配置
      */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    @Bean
+    public CorsFilter corsFilter() {
+        // 1.添加CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        // 放行哪些原始域
+        config.addAllowedOrigin("*");
+        // 是否发送Cookie信息
+        config.setAllowCredentials(true);
+        // 放行哪些原始域(请求方式)
+        config.addAllowedMethod("*");
+        // 放行哪些原始域(头部信息)
+        config.addAllowedHeader("*");
+        // 暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+        config.addExposedHeader("*");
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+        return new CorsFilter(configSource);
     }
 
     /**
