bea5cbc3aa95eb51d540cf01ed165f22211a1d3c
minghauqiang <806001926@qq.com>
2019-10-09 14:09:57 +0800

    整理各个pom文件

--- hutu-gateway/src/main/java/com/hutu/gateway/HutuGatewayApplication.java
+++ hutu-gateway/src/main/java/com/hutu/gateway/HutuGatewayApplication.java
@@ -1,13 +1,9 @@
 package com.hutu.gateway;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class HutuGatewayApplication {
