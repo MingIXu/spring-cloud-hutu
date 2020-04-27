7c4bb2ce625afa3c2c02891dff858e35f736c0f5
minghauqiang <806001926@qq.com>
2020-04-27 19:06:55 +0800

    加密解密二次封装

--- hutu-common/hutu-common-core/src/main/java/com/hutu/common/utils/SignHelper.java
+++ hutu-common/hutu-common-core/src/main/java/com/hutu/common/utils/SignHelper.java
@@ -3,6 +3,7 @@ package com.hutu.common.utils;
 
 import cn.hutool.core.codec.Base64;
 import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.asymmetric.RSA;
 import cn.hutool.crypto.asymmetric.Sign;
 import cn.hutool.crypto.asymmetric.SignAlgorithm;
 
@@ -12,7 +13,7 @@ import cn.hutool.crypto.asymmetric.SignAlgorithm;
  * @date 2018/3/28 11:41
  */
 public class SignHelper {
-    private static String PublicKe = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCcKVz9+3Kpspz8ajwF3CZ7XqsA1lSLHK46EG4ckkacWXfDO/z8jXaDHhooAj9ydIZm4dHOEIa65//S2R7n55xg2Fb2kxGmdRYZJn+Y37KhXQu6JokYPG6G1f3F7IXVsrH8KyYs4q4eeCn1zC+itunSQgMB2xt2wzH27gcpfm9N9QIDAQAB";
+    private static String PublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCcKVz9+3Kpspz8ajwF3CZ7XqsA1lSLHK46EG4ckkacWXfDO/z8jXaDHhooAj9ydIZm4dHOEIa65//S2R7n55xg2Fb2kxGmdRYZJn+Y37KhXQu6JokYPG6G1f3F7IXVsrH8KyYs4q4eeCn1zC+itunSQgMB2xt2wzH27gcpfm9N9QIDAQAB";
     private static String PrivateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJwpXP37cqmynPxqPAXcJnteqwDWVIscrjoQbhySRpxZd8M7/PyNdoMeGigCP3J0hmbh0c4Qhrrn/9LZHufnnGDYVvaTEaZ1Fhkmf5jfsqFdC7omiRg8bobV/cXshdWysfwrJizirh54KfXML6K26dJCAwHbG3bDMfbuByl+b031AgMBAAECgYBWJwvCo0JzFLM/HmzW3snsLiIxaA1kYgJMzluxL5H9F+7WqvW7b1/+nlDX2cWfI2IyyEqzYLKz0uJ0NIy0bDb86dC4IdwRQWWZcNa9iFRJVC0S7suOtgOGmrXeajBoRM9BlFAKwpjGGGZDB9wYc4oxA3sKM8RGxdSzGlm450wN0QJBANTj/GRa7IJ8rOp3fTXIoumknCbkEHI3wqNoT8OoqO0kR9T33fdxCEeMul/oj3DWEImgPFAxuxWFE5bqTU+TZUMCQQC7yJu9VyzWp2TExIQB7NB7OxyOHRNVgzsMSh4p5FKoVDHGzgygnTvn92mZxLyOIyGpkryZ9m58oDi8qydXdzBnAkEAmZZ1cy1JrgYmde2IrrG2Htu8MC6fUte5m0xZJ25ZmORw0kuUnry0XXJAz4qnWZ+GRNQOT0jhkO/2Jw2Ygw1yowJBAIoguyZgTHQst3vhjbSYzJYI2i1TB9i76iBVGLD56S82l1LEWBeA8QLlRAE+7O+kuesxK5gY5Ba6CGIHh63X13sCQBxinxfi5uVXsD1ozwApw+0QImSpgh3NpYlNSemciFi3UnQMRLugkjDNp5C8U8cVcyC3NPMDUjSosNjB3zKviyo=";
 
     /**
@@ -22,7 +23,7 @@ public class SignHelper {
      * @return
      */
     public static boolean verify(String tokenStr,String signStr){
-        Sign sign = SecureUtil.sign(SignAlgorithm.MD5withRSA,null,PublicKe);
+        Sign sign = SecureUtil.sign(SignAlgorithm.MD5withRSA,null,PublicKey);
         byte[] password0 = Base64.decode(tokenStr);
         byte[] result0 = Base64.decode(signStr);
         //验证签名
@@ -46,13 +47,11 @@ public class SignHelper {
      * 签名测试
      * @param args
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // 生成随机公钥，私钥
-//        Sign sign1 = SecureUtil.sign(SignAlgorithm.MD5withRSA);
-//        String publicKeyBase64 = sign1.getPublicKeyBase64();
-//        String privateKeyBase64 = sign1.getPrivateKeyBase64();
-//        System.out.println(publicKeyBase64);
-//        System.out.println(privateKeyBase64);
+        Sign sign1 = SecureUtil.sign(SignAlgorithm.MD5withRSA);
+        String publicKeyBase64 = sign1.getPublicKeyBase64();
+        String privateKeyBase64 = sign1.getPrivateKeyBase64();
 
         String before = Base64.encode("ming");
         String after = Base64.decodeStr(before);
@@ -60,5 +59,5 @@ public class SignHelper {
         String sign = SignHelper.sign(before);
         boolean b = SignHelper.verify(before,sign);
         System.out.println(b);
-    }
+    }*/
 }
