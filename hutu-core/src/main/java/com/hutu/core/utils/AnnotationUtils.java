571be27f11e7eca4217e49a464f2a46d25fcaabe
minghuaqiang <806001926@qq.com>
2020-11-04 17:45:23 +0800

    add 缓存本地刷新事件通知机制，优化部分工具类

--- hutu-core/src/main/java/com/hutu/core/utils/AnnotationUtils.java
+++ hutu-core/src/main/java/com/hutu/core/utils/AnnotationUtils.java
@@ -1,26 +1,59 @@
 package com.hutu.core.utils;
 
+import cn.hutool.core.annotation.AnnotationUtil;
+import cn.hutool.core.util.ReflectUtil;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * 获取类属性上的注解信息
+ *
  * @author hutu
  * @date 2018/6/8 9:46
  */
-public class AnnotationUtils {
+public class AnnotationUtils extends AnnotationUtil {
     /**
-     * 获取类指定属性上的注解值
-     * @param clazz 目标类
-     * @param fieldName 目标属性
+     * 获取类指定属性上的注解
+     *
+     * @param clazz           目标类
+     * @param fieldName       目标属性
      * @param annotationClazz 目标注解类
      * @return 注解实例
      */
-    public static Object getAnnotationValue(Class clazz, String fieldName, Class<? extends Annotation> annotationClazz) {
+    public static Object getFieldAnnotation(Class clazz, String fieldName, Class<? extends Annotation> annotationClazz) {
         Field field;
         try {
             field = clazz.getDeclaredField(fieldName);
             return field.getAnnotation(annotationClazz);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取类指定属性上的注解值
+     *
+     * @param clazz           目标类
+     * @param fieldName       目标属性
+     * @param annotationClazz 目标注解类
+     * @return 注解实例
+     */
+    public static Object getFieldAnnotationValue(Class clazz, String fieldName, Class<? extends Annotation> annotationClazz) {
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            Annotation annotation = field.getAnnotation(annotationClazz);
+            if (null == annotation) {
+                return null;
+            }
+
+            final Method method = ReflectUtil.getMethodOfObj(annotation, "value");
+            if (null == method) {
+                return null;
+            }
+            return ReflectUtil.invoke(annotation, method);
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
--- hutu-core/src/main/java/com/hutu/core/utils/DateUtils.java
+++ hutu-core/src/main/java/com/hutu/core/utils/DateUtils.java
@@ -3,6 +3,9 @@ package com.hutu.core.utils;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
 
 /**
  * 时间工具类
@@ -11,6 +14,50 @@ import java.time.ZoneId;
  * @date 2020/6/22 4:01 下午
  */
 public class DateUtils extends cn.hutool.core.date.DateUtil {
+
+    /**
+     * 更多时间格式请使用 {@link cn.hutool.core.date.DatePattern}
+     */
+
+    public static final String PATTERN_DATETIME = "yyyy-MM-dd HH:mm:ss";
+    public static final String PATTERN_DATE = "yyyy-MM-dd";
+    public static final String PATTERN_TIME = "HH:mm:ss";
+    /**
+     * java 8 时间格式化
+     */
+    public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern(DateUtils.PATTERN_DATETIME);
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DateUtils.PATTERN_DATE);
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(DateUtils.PATTERN_TIME);
+
+    /**
+     * java8 日期时间格式化
+     *
+     * @param temporal 时间
+     * @return 格式化后的时间
+     */
+    public static String formatDateTime(TemporalAccessor temporal) {
+        return DATETIME_FORMATTER.format(temporal);
+    }
+
+    /**
+     * java8 日期时间格式化
+     *
+     * @param temporal 时间
+     * @return 格式化后的时间
+     */
+    public static String formatDate(TemporalAccessor temporal) {
+        return DATE_FORMATTER.format(temporal);
+    }
+
+    /**
+     * java8 时间格式化
+     *
+     * @param temporal 时间
+     * @return 格式化后的时间
+     */
+    public static String formatTime(TemporalAccessor temporal) {
+        return TIME_FORMATTER.format(temporal);
+    }
 
     /**
      * 将时间戳转化为 LocalDateTime
